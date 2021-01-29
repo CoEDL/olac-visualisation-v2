d97cf90389a293e898f7350c01552be9b393285c
Marco La Rosa <m@lr.id.au>
2021-01-29 13:28:00 +1100

    implement handlers / tests to extract languages and language data

--- data-processor/lib.js
+++ data-processor/lib.js
@@ -1,52 +1,164 @@
 const fetch = require("node-fetch");
 const path = require("path");
-const { readFile, writeFile, ensureDir } = require("fs-extra");
+const { readFile, writeFile, readdir, ensureDir } = require("fs-extra");
 const xpath = require("xpath");
-const dom = require("xmldom").DOMParser;
+const { trim, compact } = require("lodash");
+let dom = require("xmldom").DOMParser;
+dom = new dom({
+    locator: {},
+    errorHandler: {
+        warning: () => {},
+        error: (e) => console.log(e),
+        fatalError: (e) => console.log(e),
+    },
+});
+
+const siteUrl = "http://www.language-archives.org";
 const areaDataFiles = [
-    "http://www.language-archives.org/area/africa",
-    "http://www.language-archives.org/area/americas",
-    "http://www.language-archives.org/area/asia",
-    "http://www.language-archives.org/area/europe",
-    "http://www.language-archives.org/area/pacific",
+    "/area/africa",
+    "/area/americas",
+    "/area/asia",
+    "/area/europe",
+    "/area/pacific",
 ];
 
 module.exports = {
-    downloadAreaDataFiles,
+    downloadFile,
     extractAreaCountries,
+    extractAreaData,
+    downloadCountryDataFiles,
+    downloadLanguageDataFiles,
+    extractCountryLanguages,
+    extractLanguageData,
 };
 
 async function downloadAreaDataFiles({ folder }) {
-    await ensureDir(folder);
     for (let url of areaDataFiles) {
-        let response = await fetch(url);
-        if (response.status !== 200) {
-            console.log(`Error getting ${url}`);
-            continue;
-        }
-        const data = await response.text();
-        const area = path.join(folder, `${path.basename(url)}.html`);
-        await writeFile(area, data);
+        url = `${siteUrl}${url}`;
+        await downloadFile({ url, folder });
     }
 }
 
-async function extractAreaCountries({ folder }) {
+async function downloadCountryDataFiles({ folder, countries }) {
+    await ensureDir(folder);
+    for (let country of countries) {
+        await downloadFile({ folder, url: country.dataFile });
+    }
+}
+
+async function downloadLanguageDataFiles({ folder, languages }) {
+    await ensureDir(folder);
+    for (let language of languages) {
+        await downloadFile({ folder, url: language.dataFile });
+    }
+}
+
+async function downloadFile({ url, folder }) {
+    await ensureDir(folder);
+    let response = await fetch(url);
+    if (response.status !== 200) {
+        console.log(`Error getting ${url}`);
+        return;
+    }
+    const data = await response.text();
+    const file = path.join(folder, `${path.basename(url)}.html`);
+    await writeFile(file, data);
+}
+
+async function extractAreaData({ folder }) {
     let countryData = {};
-    for (let url of areaDataFiles) {
-        const area = path.join(folder, `${path.basename(url)}.html`);
-        const data = await readFile(area);
-        const doc = new dom().parseFromString(data.toString());
-        //body/table[@class="doc_header"]/
-        let nodes = xpath.select("//body/table[2]/tr/td/ul/li", doc);
-        // console.log(nodes[0]);
-        let countries = nodes.map((node) => {
-            let country = {
-                name: xpath.select("string(a)", node),
-                dataFile: xpath.select("string(a/@href)", node),
-            };
-            return country;
-        });
-        countryData[path.basename(url)] = countries;
+    for (let file of await readdir(folder)) {
+        let country = path.basename(file, ".html");
+        file = path.join(folder, file);
+        let countries = await extractAreaCountries({ file });
+        countryData[country] = countries;
     }
     return countryData;
+}
+
+async function extractAreaCountries({ file }) {
+    const data = await readFile(file);
+    const doc = dom.parseFromString(data.toString());
+    let nodes = xpath.select("//body/table[2]/tr/td/ul/li", doc);
+    let countries = nodes.map((node) => {
+        let country = {
+            name: xpath.select("string(a)", node),
+            dataFile: `${siteUrl}${xpath.select("string(a/@href)", node)}`,
+        };
+        return country;
+    });
+    return countries;
+}
+
+async function extractCountryLanguages({ file }) {
+    const data = await readFile(file);
+    const doc = dom.parseFromString(data.toString());
+    let nodes = xpath.select("//body/table[2]/tr/td/ul/li", doc);
+    let languages = nodes.map((node) => {
+        return {
+            name: xpath.select("string(a)", node),
+            dataFile: `${siteUrl}${xpath.select("string(a/@href)", node)}`,
+        };
+    });
+    return languages;
+}
+
+async function extractLanguageData({ file }) {
+    const data = await readFile(file);
+    const doc = dom.parseFromString(data.toString());
+
+    let languageData = {
+        otherNamesAndDialects: getLanguageKnownNamesAndDialects({ doc }),
+        dataTypes: getDataTypes({ doc }),
+    };
+    languageData.resources = getResources({ doc, types: languageData.dataTypes });
+    return languageData;
+}
+
+function getLanguageKnownNamesAndDialects({ doc }) {
+    // try to get other known names
+    let nodes = xpath.select("//body/p", doc);
+    for (let node of nodes) {
+        if (node.textContent.match("Other known names and dialect names:")) {
+            return node.textContent
+                .split("Other known names and dialect names:")[1]
+                .split(", ")
+                .map((l) => trim(l));
+        }
+    }
+}
+
+function getDataTypes({ doc }) {
+    let nodes = xpath.select("//body/h2", doc);
+    let dataTypes = nodes.map((node) => node.textContent);
+    return dataTypes;
+}
+
+function getResources({ doc, types }) {
+    let resources = {};
+    for (let type of types) {
+        resources[type] = [];
+        let nodes = xpath.select(`//body/h2[text()='${type}']/following-sibling::*[1]/li`, doc);
+        for (let node of nodes) {
+            let isOnline = xpath.select("string(span)", node) === "ONLINE" ? true : false;
+            let resourceLink = {
+                name: xpath.select("string(a)", node),
+                url: `${siteUrl}${xpath.select("string(a/@href)", node)}`,
+            };
+
+            let resourceName = [];
+            for (let child of Object.keys(node.childNodes)) {
+                child = node.childNodes[child];
+                if (["a", "span"].includes(child.nodeName)) continue;
+                resourceName.push(child.textContent);
+            }
+            resourceName = trim(compact(resourceName).join(" "));
+            resources[type].push({
+                isOnline,
+                resourceName,
+                resourceLink,
+            });
+        }
+    }
+    return resources;
 }
--- data-processor/lib.spec.js
+++ data-processor/lib.spec.js
@@ -1,18 +1,88 @@
-const { downloadAreaDataFiles, extractAreaCountries } = require("./lib");
+const {
+    downloadFile,
+    downloadCountryDataFiles,
+    downloadLanguageDataFiles,
+    extractAreaCountries,
+    extractAreaData,
+    extractCountryLanguages,
+    extractLanguageData,
+} = require("./lib");
 const path = require("path");
 const { ensureFile } = require("fs-extra");
 
-it.skip(`should be able to download area datafiles`, async () => {
-    const folder = path.join(__dirname, "data");
-    await downloadAreaDataFiles({ folder });
+it(`should be able to download area datafiles`, async () => {
+    const folder = path.join(__dirname, "data", "area");
+    await downloadFile({
+        url: "http://www.language-archives.org/area/africa",
+        folder,
+    });
     expect(await ensureFile(path.join(folder, "africa.html"))).toBeTrue;
-    expect(await ensureFile(path.join(folder, "americas.html"))).toBeTrue;
-    expect(await ensureFile(path.join(folder, "asia.html"))).toBeTrue;
-    expect(await ensureFile(path.join(folder, "europe.html"))).toBeTrue;
-    expect(await ensureFile(path.join(folder, "pacific.html"))).toBeTrue;
 });
-it(`should be able to extract area languages`, async () => {
-    const folder = path.join(__dirname, "data");
-    let countryData = await extractAreaCountries({ folder });
-    expect(Object.keys(countryData)).toEqual(["africa", "americas", "asia", "europe", "pacific"]);
+it(`should be able to extract area countries`, async () => {
+    const folder = path.join(__dirname, "data", "area");
+
+    await downloadFile({
+        url: "http://www.language-archives.org/area/africa",
+        folder,
+    });
+
+    let countries = await extractAreaCountries({
+        file: path.join(folder, "africa.html"),
+    });
+    expect(countries.length).toEqual(55);
+
+    let countryData = await extractAreaData({ folder });
+    expect(Object.keys(countryData)).toEqual(["africa"]);
+});
+it(`should be able to download country data files`, async () => {
+    let folder = path.join(__dirname, "data", "countries");
+    let countries = [
+        {
+            name: "Algeria",
+            dataFile: "http://www.language-archives.org/country/DZ",
+        },
+    ];
+    await downloadCountryDataFiles({ folder, countries });
+
+    let country = path.basename(countries[0].dataFile);
+    expect(await ensureFile(path.join(folder, `${country}.html`))).toBeTrue;
+});
+it(`should be able to download language data files`, async () => {
+    let folder = path.join(__dirname, "data", "languages");
+    let languages = [
+        {
+            name: "Algerian Arabic [arq]",
+            dataFile: "http://www.language-archives.org/language/arq",
+        },
+    ];
+    await downloadLanguageDataFiles({ folder, languages });
+    let language = path.basename(languages[0].dataFile);
+    expect(await ensureFile(path.join(folder, `${language}.html`))).toBeTrue;
+});
+it("should be able to extract country languages", async () => {
+    let folder = path.join(__dirname, "data", "countries");
+    let url = "http://www.language-archives.org/country/DZ";
+    await downloadFile({ folder, url });
+    let file = path.join(folder, "DZ.html");
+    let languages = await extractCountryLanguages({ file });
+    expect(languages.length).toEqual(17);
+});
+it("should be able to extract language data", async () => {
+    let folder = path.join(__dirname, "data", "languages");
+    let url = "http://www.language-archives.org/language/arq";
+    await downloadFile({ folder, url });
+
+    let file = path.join(folder, "arq.html");
+    let data = await extractLanguageData({ file });
+    expect(Object.keys(data)).toEqual(["otherNamesAndDialects", "dataTypes", "resources"]);
+    expect(data.dataTypes).toEqual([
+        "Primary texts",
+        "Lexical resources",
+        "Language descriptions",
+        "Other resources about the language",
+    ]);
+    expect(data.resources["Primary texts"].length).toEqual(1);
+    expect(data.resources["Lexical resources"].length).toEqual(1);
+    expect(data.resources["Language descriptions"].length).toEqual(2);
+    expect(data.resources["Other resources about the language"].length).toEqual(5);
 });
