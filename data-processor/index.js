331fc300fa59b8d1da214fa053df2dc2f899f106
Marco La Rosa <m@lr.id.au>
2021-02-04 11:12:45 +1100

    formatting; only store code and total with lang geojson

--- data-processor/index.js
+++ data-processor/index.js
@@ -1,155 +1,173 @@
 const {
-    downloadAreaDataFiles,
-    downloadFile,
-    downloadCountryDataFiles,
-    extractAreaData,
-    extractCountryLanguages,
-    extractLanguageData,
-    loadLanguageCoordinateData,
-    loadCountryInformationDatasets,
+  downloadAreaDataFiles,
+  downloadFile,
+  downloadCountryDataFiles,
+  extractAreaData,
+  extractCountryLanguages,
+  extractLanguageData,
+  loadLanguageCoordinateData,
+  loadCountryInformationDatasets,
 } = require("./lib");
 const path = require("path");
-const { readFile, writeFile, readdir, readJSON, writeJSON, ensureDir } = require("fs-extra");
+const {
+  readFile,
+  writeFile,
+  readdir,
+  readJSON,
+  writeJSON,
+  ensureDir,
+} = require("fs-extra");
 const { trim } = require("lodash");
 const configuration = require("./configuration");
 
 const args = require("yargs/yargs")(process.argv.slice(2)).options({
-    folder: {
-        demandOption: true,
-        describe: "The location to store the datafiles",
-    },
-    verbose: {
-        default: false,
-        type: "boolean",
-    },
-    download: {
-        default: true,
-        type: "boolean",
-    },
-    extract: {
-        default: true,
-        type: "boolean",
-    },
+  folder: {
+    demandOption: true,
+    describe: "The location to store the datafiles",
+  },
+  verbose: {
+    default: false,
+    type: "boolean",
+  },
+  download: {
+    default: true,
+    type: "boolean",
+  },
+  extract: {
+    default: true,
+    type: "boolean",
+  },
 }).argv;
 
 (async () => {
-    if (!args.folder) {
-        console.log(`--folder required`);
-    }
-    const verbose = args.verbose;
-    const dataBasePath = args.folder;
+  if (!args.folder) {
+    console.log(`--folder required`);
+  }
+  const verbose = args.verbose;
+  const dataBasePath = args.folder;
 
-    const areaDataPath = path.join(dataBasePath, "area");
-    const countryDataPath = path.join(dataBasePath, "countries");
-    const languageDataPath = path.join(dataBasePath, "languages");
-    const indexesDataPath = path.join(dataBasePath, "indexes");
+  const areaDataPath = path.join(dataBasePath, "area");
+  const countryDataPath = path.join(dataBasePath, "countries");
+  const languageDataPath = path.join(dataBasePath, "languages");
+  const indexesDataPath = path.join(dataBasePath, "indexes");
 
-    await ensureDir(areaDataPath);
-    await ensureDir(countryDataPath);
-    await ensureDir(languageDataPath);
-    await ensureDir(indexesDataPath);
+  await ensureDir(areaDataPath);
+  await ensureDir(countryDataPath);
+  await ensureDir(languageDataPath);
+  await ensureDir(indexesDataPath);
 
-    // get world country data files - data and geojson
-    await loadCountryInformationDatasets({ dataBasePath });
+  // get world country data files - data and geojson
+  await loadCountryInformationDatasets({ dataBasePath });
 
-    // load language coordinate info
-    const languageCoordinateData = await loadLanguageCoordinateData();
+  // load language coordinate info
+  const languageCoordinateData = await loadLanguageCoordinateData();
 
-    // get area data files and extract countries list
-    if (args.download) await downloadAreaDataFiles({ folder: areaDataPath, verbose });
-    let countries = await extractAreaData({ folder: areaDataPath });
+  // get area data files and extract countries list
+  if (args.download)
+    await downloadAreaDataFiles({ folder: areaDataPath, verbose });
+  let countries = await extractAreaData({ folder: areaDataPath });
 
-    // get country data files
-    if (args.download) {
-        await downloadCountryDataFiles({
-            folder: countryDataPath,
-            countries,
-            verbose,
-        });
-    }
-
-    // extract country languages
-    let languageDataFiles = {};
-    let files = await readdir(countryDataPath);
-    for (let file of files) {
-        let countryCode = path.basename(file, ".html");
-        let languages = await extractCountryLanguages({
-            file: path.join(countryDataPath, file),
-        });
-        languages.forEach((language) => {
-            languageDataFiles[language.code] = language;
-        });
-        countries = countries.map((country) => {
-            if (country.code === countryCode) country.languages = languages;
-            return country;
-        });
-    }
-
-    // download language data files and extract the data
-    for (let code of Object.keys(languageDataFiles).sort()) {
-        let language = languageDataFiles[code];
-        if (args.download) {
-            await downloadFile({
-                folder: path.join(languageDataPath, "html"),
-                url: language.dataFile,
-                verbose,
-            });
-        }
-
-        let htmlFile = path.join(languageDataPath, "html", `${code}.html`);
-        let jsonFile = path.join(languageDataPath, "json", `${code}.json`);
-
-        if (args.extract) {
-            if (verbose) console.log(`Extracting data ${htmlFile}`);
-            let data = await extractLanguageData({ file: htmlFile });
-
-            let coords = languageCoordinateData[code];
-            if (!coords) {
-                console.log(`ERROR: Couldn't find coordinates for ${code}`);
-            } else {
-                coords.properties.name = languageDataFiles[code].name;
-                coords.properties.dataFile = languageDataFiles[code].dataFile;
-                coords.properties.totalResources = data.totalResources;
-                coords.properties.summary = data.summary;
-                data = {
-                    ...data,
-                    ...languageDataFiles[code],
-                    geojson: coords,
-                };
-            }
-            await writeFile(jsonFile, JSON.stringify(data));
-        }
-    }
-
-    // write data indexes
-
-    //   write countries index
-    let countriesGeoJSON = await readJSON(path.join(dataBasePath, "countries.geojson"));
-    let countryCodesJSON = await readJSON(path.join(dataBasePath, "country-codes.json"));
-    countries = countries.map((country) => {
-        let countryData = countryCodesJSON.filter((c) => c["alpha-2"] === country.code)[0];
-        let countryJSON = countriesGeoJSON.features.filter((c) => {
-            return c.properties["ISO_A3"] === countryData["alpha-3"];
-        })[0];
-        country = {
-            ...country,
-            ...countryData,
-            geojson: countryJSON,
-        };
-        if (!country.geojson) {
-            console.log(`ERROR: Couldn't find bounds for ${country.name}, ${country.code}`);
-        }
-        return country;
+  // get country data files
+  if (args.download) {
+    await downloadCountryDataFiles({
+      folder: countryDataPath,
+      countries,
+      verbose,
     });
-    await writeJSON(path.join(indexesDataPath, "countries.json"), countries);
+  }
 
-    //  write languages index
-    files = await readdir(path.join(languageDataPath, "json"));
-    let languages = [];
-    for (let file of files) {
-        let data = await readJSON(path.join(languageDataPath, "json", file));
-        languages.push(data.geojson);
+  // extract country languages
+  let languageDataFiles = {};
+  let files = await readdir(countryDataPath);
+  for (let file of files) {
+    let countryCode = path.basename(file, ".html");
+    let languages = await extractCountryLanguages({
+      file: path.join(countryDataPath, file),
+    });
+    languages.forEach(language => {
+      languageDataFiles[language.code] = language;
+    });
+    countries = countries.map(country => {
+      if (country.code === countryCode) country.languages = languages;
+      return country;
+    });
+  }
+
+  // download language data files and extract the data
+  for (let code of Object.keys(languageDataFiles).sort()) {
+    let language = languageDataFiles[code];
+    if (args.download) {
+      await downloadFile({
+        folder: path.join(languageDataPath, "html"),
+        url: language.dataFile,
+        verbose,
+      });
     }
-    await writeJSON(path.join(indexesDataPath, "languages.json"), languages);
-    process.exit();
+
+    let htmlFile = path.join(languageDataPath, "html", `${code}.html`);
+    let jsonFile = path.join(languageDataPath, "json", `${code}.json`);
+
+    if (args.extract) {
+      if (verbose) console.log(`Extracting data ${htmlFile}`);
+      let data = await extractLanguageData({ file: htmlFile });
+
+      let coords = languageCoordinateData[code];
+      if (!coords) {
+        console.log(`ERROR: Couldn't find coordinates for ${code}`);
+      } else {
+        // coords.properties.name = languageDataFiles[code].name;
+        // coords.properties.dataFile = languageDataFiles[code].dataFile;
+        // coords.properties.totalResources = data.totalResources;
+        coords.properties.total = data.totalResources;
+        // coords.properties.summary = data.summary;
+        data = {
+          ...data,
+          ...languageDataFiles[code],
+          geojson: coords,
+        };
+      }
+      await writeFile(jsonFile, JSON.stringify(data));
+    }
+  }
+
+  // write data indexes
+
+  //   write countries index
+  let countriesGeoJSON = await readJSON(
+    path.join(dataBasePath, "countries.geojson")
+  );
+  let countryCodesJSON = await readJSON(
+    path.join(dataBasePath, "country-codes.json")
+  );
+  countries = countries.map(country => {
+    let countryData = countryCodesJSON.filter(
+      c => c["alpha-2"] === country.code
+    )[0];
+    let countryJSON = countriesGeoJSON.features.filter(c => {
+      return c.properties["ISO_A3"] === countryData["alpha-3"];
+    })[0];
+    country = {
+      ...country,
+      ...countryData,
+      geojson: countryJSON,
+    };
+    if (!country.geojson) {
+      console.log(
+        `ERROR: Couldn't find bounds for ${country.name}, ${country.code}`
+      );
+    }
+    return country;
+  });
+  await writeJSON(path.join(indexesDataPath, "countries.json"), countries);
+
+  //  write languages index
+  files = await readdir(path.join(languageDataPath, "json"));
+  let languages = [];
+  for (let file of files) {
+    let data = await readJSON(path.join(languageDataPath, "json", file));
+    if (data.geojson?.geometry?.coordinates.length)
+      languages.push(data.geojson);
+  }
+  await writeJSON(path.join(indexesDataPath, "languages.json"), languages);
+  process.exit();
 })();
--- data-processor/lib.js
+++ data-processor/lib.js
@@ -1,242 +1,263 @@
 const fetch = require("node-fetch");
 const path = require("path");
-const { readFile, writeFile, readdir, ensureDir, pathExists, stat } = require("fs-extra");
+const {
+  readFile,
+  writeFile,
+  readdir,
+  ensureDir,
+  pathExists,
+  stat,
+} = require("fs-extra");
 const xpath = require("xpath");
 const { trim, compact, flattenDeep, sum } = require("lodash");
 const configuration = require("./configuration");
 let dom = require("xmldom").DOMParser;
 dom = new dom({
-    locator: {},
-    errorHandler: {
-        warning: () => {},
-        error: (e) => console.log(e),
-        fatalError: (e) => console.log(e),
-    },
+  locator: {},
+  errorHandler: {
+    warning: () => {},
+    error: e => console.log(e),
+    fatalError: e => console.log(e),
+  },
 });
 
 const siteUrl = "http://www.language-archives.org";
 const areaDataFiles = [
-    "/area/africa",
-    "/area/americas",
-    "/area/asia",
-    "/area/europe",
-    "/area/pacific",
+  "/area/africa",
+  "/area/americas",
+  "/area/asia",
+  "/area/europe",
+  "/area/pacific",
 ];
 
 module.exports = {
-    downloadFile,
-    extractAreaCountries,
-    extractAreaData,
-    downloadAreaDataFiles,
-    downloadCountryDataFiles,
-    downloadLanguageDataFiles,
-    extractCountryLanguages,
-    extractLanguageData,
-    loadLanguageCoordinateData,
-    loadCountryInformationDatasets,
+  downloadFile,
+  extractAreaCountries,
+  extractAreaData,
+  downloadAreaDataFiles,
+  downloadCountryDataFiles,
+  downloadLanguageDataFiles,
+  extractCountryLanguages,
+  extractLanguageData,
+  loadLanguageCoordinateData,
+  loadCountryInformationDatasets,
 };
 
 async function downloadAreaDataFiles({ folder, verbose }) {
-    for (let url of areaDataFiles) {
-        url = `${siteUrl}${url}`;
-        await downloadFile({ url, folder, verbose });
-    }
+  for (let url of areaDataFiles) {
+    url = `${siteUrl}${url}`;
+    await downloadFile({ url, folder, verbose });
+  }
 }
 
 async function downloadCountryDataFiles({ folder, countries, verbose }) {
-    await ensureDir(folder);
-    for (let country of countries) {
-        await downloadFile({ folder, url: country.dataFile, verbose });
-    }
+  await ensureDir(folder);
+  for (let country of countries) {
+    await downloadFile({ folder, url: country.dataFile, verbose });
+  }
 }
 
 async function downloadLanguageDataFiles({ folder, languages }) {
-    await ensureDir(folder);
-    for (let language of languages) {
-        await downloadFile({ folder, url: language.dataFile });
-    }
+  await ensureDir(folder);
+  for (let language of languages) {
+    await downloadFile({ folder, url: language.dataFile });
+  }
 }
 
 async function downloadFile({ url, name, folder, verbose = false }) {
-    const file = name ? path.join(folder, name) : path.join(folder, `${path.basename(url)}.html`);
-    if (await pathExists(file)) {
-        // if file is less than one day old don't re download
-        let stats = await stat(file);
-        if ((Date.now() - stats.mtime) / 1000 < configuration.maxDataLifetime) return;
-    }
+  const file = name
+    ? path.join(folder, name)
+    : path.join(folder, `${path.basename(url)}.html`);
+  if (await pathExists(file)) {
+    // if file is less than one day old don't re download
+    let stats = await stat(file);
+    if ((Date.now() - stats.mtime) / 1000 < configuration.maxDataLifetime)
+      return;
+  }
 
-    if (verbose) console.log(`Downloading ${url}`);
-    await ensureDir(folder);
-    let response = await fetch(url);
-    if (response.status !== 200) {
-        console.log(`Error getting ${url}`);
-        return;
-    }
-    const data = await response.text();
-    await writeFile(file, data);
+  if (verbose) console.log(`Downloading ${url}`);
+  await ensureDir(folder);
+  let response = await fetch(url);
+  if (response.status !== 200) {
+    console.log(`Error getting ${url}`);
+    return;
+  }
+  const data = await response.text();
+  await writeFile(file, data);
 }
 
 async function extractAreaData({ folder }) {
-    let countryData = [];
-    for (let file of await readdir(folder)) {
-        // let area = path.basename(file, ".html");
-        file = path.join(folder, file);
-        let countries = await extractAreaCountries({ file });
-        countryData.push(countries);
-    }
-    countryData = flattenDeep(countryData);
-    return countryData;
+  let countryData = [];
+  for (let file of await readdir(folder)) {
+    // let area = path.basename(file, ".html");
+    file = path.join(folder, file);
+    let countries = await extractAreaCountries({ file });
+    countryData.push(countries);
+  }
+  countryData = flattenDeep(countryData);
+  return countryData;
 }
 
 async function extractAreaCountries({ file }) {
-    const data = await readFile(file);
-    const doc = dom.parseFromString(data.toString());
-    let nodes = xpath.select("//body/table[2]/tr/td/ul/li", doc);
-    let countries = nodes.map((node) => {
-        let country = {
-            code: `${path.basename(xpath.select("string(a/@href)", node))}`,
-            name: xpath.select("string(a)", node),
-            dataFile: `${siteUrl}${xpath.select("string(a/@href)", node)}`,
-        };
-        return country;
-    });
-    return countries;
+  const data = await readFile(file);
+  const doc = dom.parseFromString(data.toString());
+  let nodes = xpath.select("//body/table[2]/tr/td/ul/li", doc);
+  let countries = nodes.map(node => {
+    let country = {
+      code: `${path.basename(xpath.select("string(a/@href)", node))}`,
+      name: xpath.select("string(a)", node),
+      dataFile: `${siteUrl}${xpath.select("string(a/@href)", node)}`,
+    };
+    return country;
+  });
+  return countries;
 }
 
 async function extractCountryLanguages({ file }) {
-    const data = await readFile(file);
-    const doc = dom.parseFromString(data.toString());
-    let nodes = xpath.select("//body/table[2]/tr/td/ul/li", doc);
-    let languages = nodes.map((node) => {
-        return {
-            name: xpath.select("string(a)", node),
-            code: path.basename(xpath.select("string(a/@href)", node)),
-            dataFile: `${siteUrl}${xpath.select("string(a/@href)", node)}`,
-        };
-    });
-    return languages;
+  const data = await readFile(file);
+  const doc = dom.parseFromString(data.toString());
+  let nodes = xpath.select("//body/table[2]/tr/td/ul/li", doc);
+  let languages = nodes.map(node => {
+    return {
+      name: xpath.select("string(a)", node),
+      code: path.basename(xpath.select("string(a/@href)", node)),
+      dataFile: `${siteUrl}${xpath.select("string(a/@href)", node)}`,
+    };
+  });
+  return languages;
 }
 
 async function extractLanguageData({ file }) {
-    const code = path.basename(file, ".html");
-    const data = await readFile(file);
-    const doc = dom.parseFromString(data.toString());
+  const code = path.basename(file, ".html");
+  const data = await readFile(file);
+  const doc = dom.parseFromString(data.toString());
 
-    let languageData = {
-        code,
-        name: getLanguageName({ doc }),
-        otherNamesAndDialects: getLanguageKnownNamesAndDialects({ doc }),
-        dataTypes: getDataTypes({ doc }),
-    };
-    languageData.resources = getResources({
-        doc,
-        types: languageData.dataTypes,
-    });
-    languageData.summary = {};
-    for (let resource of Object.keys(languageData.resources)) {
-        languageData.summary[resource] = languageData.resources[resource].length;
-    }
-    languageData.totalResources = sum(
-        Object.keys(languageData.resources).map((resource) => languageData.summary[resource])
-    );
-    return languageData;
+  let languageData = {
+    code,
+    name: getLanguageName({ doc }),
+    otherNamesAndDialects: getLanguageKnownNamesAndDialects({ doc }),
+    dataTypes: getDataTypes({ doc }),
+  };
+  languageData.resources = getResources({
+    doc,
+    types: languageData.dataTypes,
+  });
+  languageData.summary = {};
+  for (let resource of Object.keys(languageData.resources)) {
+    languageData.summary[resource] = languageData.resources[resource].length;
+  }
+  languageData.totalResources = sum(
+    Object.keys(languageData.resources).map(
+      resource => languageData.summary[resource]
+    )
+  );
+  if (!languageData.totalResources) {
+    console.log(`ERROR: ${code} has no resources`);
+  }
+  return languageData;
 }
 
 function getLanguageName({ doc }) {
-    let node = xpath.select("//body/table[1]/tr/td[2]", doc)[0];
-    return node.textContent;
+  let node = xpath.select("//body/table[1]/tr/td[2]", doc)[0];
+  return node.textContent;
 }
 
 function getLanguageKnownNamesAndDialects({ doc }) {
-    // try to get other known names
-    let nodes = xpath.select("//body/p", doc);
-    for (let node of nodes) {
-        if (node.textContent.match("Other known names and dialect names:")) {
-            return node.textContent
-                .split("Other known names and dialect names:")[1]
-                .split(", ")
-                .map((l) => trim(l));
-        }
+  // try to get other known names
+  let nodes = xpath.select("//body/p", doc);
+  for (let node of nodes) {
+    if (node.textContent.match("Other known names and dialect names:")) {
+      return node.textContent
+        .split("Other known names and dialect names:")[1]
+        .split(", ")
+        .map(l => trim(l));
     }
+  }
 }
 
 function getDataTypes({ doc }) {
-    let nodes = xpath.select("//body/h2", doc);
-    let dataTypes = nodes.map((node) => node.textContent);
-    return dataTypes;
+  let nodes = xpath.select("//body/h2", doc);
+  let dataTypes = nodes.map(node => node.textContent);
+  return dataTypes;
 }
 
 function getResources({ doc, types }) {
-    let resources = {};
-    for (let type of types) {
-        resources[type] = [];
-        let nodes = xpath.select(`//body/h2[text()='${type}']/following-sibling::*[1]/li`, doc);
-        for (let node of nodes) {
-            let isOnline = xpath.select("string(span)", node) === "ONLINE" ? true : false;
-            let resourceLink = {
-                name: xpath.select("string(a)", node),
-                url: `${siteUrl}${xpath.select("string(a/@href)", node)}`,
-            };
+  let resources = {};
+  for (let type of types) {
+    resources[type] = [];
+    let nodes = xpath.select(
+      `//body/h2[text()='${type}']/following-sibling::*[1]/li`,
+      doc
+    );
+    for (let node of nodes) {
+      let isOnline =
+        xpath.select("string(span)", node) === "ONLINE" ? true : false;
+      let resourceLink = {
+        name: xpath.select("string(a)", node),
+        url: `${siteUrl}${xpath.select("string(a/@href)", node)}`,
+      };
 
-            let resourceName = [];
-            for (let child of Object.keys(node.childNodes)) {
-                child = node.childNodes[child];
-                if (["a", "span"].includes(child.nodeName)) continue;
-                resourceName.push(child.textContent);
-            }
-            resourceName = trim(compact(resourceName).join(" "));
-            resources[type].push({
-                isOnline,
-                resourceName,
-                resourceLink,
-            });
-        }
+      let resourceName = [];
+      for (let child of Object.keys(node.childNodes)) {
+        child = node.childNodes[child];
+        if (["a", "span"].includes(child.nodeName)) continue;
+        resourceName.push(child.textContent);
+      }
+      resourceName = trim(compact(resourceName).join(" "));
+      resources[type].push({
+        isOnline,
+        resourceName,
+        resourceLink,
+      });
     }
-    return resources;
+  }
+  return resources;
 }
 
 async function loadLanguageCoordinateData() {
-    // load language coordinate info
-    let coordinates = await readFile("languages.csv");
-    let languageCoordinateData = {};
-    for (line of coordinates.toString().split("\n")) {
-        line = line.split(",").map((e) => trim(e));
+  // load language coordinate info
+  let coordinates = await readFile("languages.csv");
+  let languageCoordinateData = {};
+  for (line of coordinates.toString().split("\n")) {
+    line = line.split(",").map(e => trim(e));
 
-        let coords = [];
-        let code = line[0];
-        if (line.length === 10) {
-            coords = [line[8], line[9]];
-        } else if (line.length === 8) {
-            coords = [line[4], line[6]];
-        } else {
-            console.log(`ERROR: ${line} in languages.csv seems to be an unexpected length`);
-        }
-        if (code) {
-            languageCoordinateData[code] = {
-                type: "Feature",
-                geometry: {
-                    type: "Point",
-                    coordinates: coords,
-                },
-                properties: {
-                    code: code,
-                },
-            };
-        }
+    let coords = [];
+    let code = line[0];
+    if (line.length === 10) {
+      coords = [line[9], line[8]];
+    } else if (line.length === 8) {
+      coords = [line[6], line[4]];
+    } else {
+      console.log(
+        `ERROR: '${line}' in languages.csv seems to be an unexpected length`
+      );
     }
-    return languageCoordinateData;
+    if (code) {
+      languageCoordinateData[code] = {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: coords,
+        },
+        properties: {
+          code: code,
+        },
+      };
+    }
+  }
+  return languageCoordinateData;
 }
 
 async function loadCountryInformationDatasets({ dataBasePath }) {
-    await downloadFile({
-        url: configuration.country.codes,
-        folder: dataBasePath,
-        name: "country-codes.json",
-    });
+  await downloadFile({
+    url: configuration.country.codes,
+    folder: dataBasePath,
+    name: "country-codes.json",
+  });
 
-    await downloadFile({
-        url: configuration.country.geojson,
-        folder: dataBasePath,
-        name: "countries.geojson",
-    });
+  await downloadFile({
+    url: configuration.country.geojson,
+    folder: dataBasePath,
+    name: "countries.geojson",
+  });
 }
