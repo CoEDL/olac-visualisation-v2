2d024a8c751d694607132e858b5618f4a373374b
Marco La Rosa <m@lr.id.au>
2021-02-05 16:42:08 +1100

    refactor and debounce actions

--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { debounce } from "lodash";
 const repository =
   process.env.NODE_ENV === "development"
     ? "/repository"
     : "/olacvis/repository";
 
 Vue.use(Vuex);
+const debouncedLoadData = debounce(loadData, 100);
+const debouncedLoadLanguage = debounce(loadLanguage, 500);
 
 export const mutations = {
   saveCountries(state, countries) {
@@ -14,34 +17,54 @@ export const mutations = {
   saveLanguages(state, languages) {
     state.languages = [...languages];
   },
+  saveLanguage(state, language) {
+    state.selectedLanguage = { ...language };
+  },
 };
 
 export const actions = {
-  async loadData({ commit }) {
-    let response = await fetch(`${repository}/indexes/countries.json`);
-    if (response.status !== 200) {
-      console.log(`Unable to load country data`);
-      return;
-    }
-    let countries = await response.json();
-    commit("saveCountries", countries);
-
-    response = await fetch(`${repository}/indexes/languages.json`);
-    if (response.status !== 200) {
-      console.log(`Unable to load country data`);
-      return;
-    }
-    let languages = await response.json();
-    commit("saveLanguages", languages);
-  },
+  loadData: debouncedLoadData,
+  loadLanguage: debouncedLoadLanguage,
 };
 
 export default new Vuex.Store({
   state: {
     countries: [],
     languages: [],
+    selectedLanguage: {},
   },
   mutations,
   actions,
   modules: {},
 });
+
+async function loadData({ commit }) {
+  let response = await fetch(`${repository}/indexes/countries.json`);
+  if (response.status !== 200) {
+    console.log(`Unable to load country data`);
+    return;
+  }
+  let countries = await response.json();
+  commit("saveCountries", countries);
+
+  response = await fetch(`${repository}/indexes/languages.json`);
+  if (response.status !== 200) {
+    console.log(`Unable to load language data`);
+    return;
+  }
+  let languages = await response.json();
+  commit("saveLanguages", languages);
+}
+
+async function loadLanguage({ commit }, { code }) {
+  commit("saveLanguage", {});
+  if (!code) return;
+  await new Promise(resolve => setTimeout(resolve, 500));
+  let response = await fetch(`${repository}/languages/json/${code}.json`);
+  if (response.status !== 200) {
+    console.log(`Unable to load language data`);
+    return;
+  }
+  let language = await response.json();
+  commit("saveLanguage", language);
+}
