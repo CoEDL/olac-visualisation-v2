a8d62c26030fd71bad2170bddc98a4bcab536237
Marco La Rosa <m@lr.id.au>
2021-02-08 08:04:18 +1100

    rename file

---
+++ src/components/Topbar.component.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full flex flex-row p-4 space-x-2">
+    <div>
+      <img :src="logo" class="h-10" />
+    </div>
+    <div class="text-gray-700 pt-1 md:pt-2 text-xs md:test-base">
+      OLAC Language Archives data visualisation
+    </div>
+  </div>
+</template>
+
+<script>
+const logo = require("@/assets/logo.png");
+export default {
+  data() {
+    return {
+      logo,
+    };
+  },
+};
+</script>
