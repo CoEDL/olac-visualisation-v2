b52eb5a02df9ec609e128cbda2ae64736098baad
Marco La Rosa <m@lr.id.au>
2021-02-09 15:34:54 +1100

    refactor language data modal and rework search capability

--- src/components/BrowseLanguageResourcesModal.component.vue
+++ src/components/BrowseLanguageResourcesModal.component.vue
@@ -1,91 +1,43 @@
 <template>
   <div>
-    <ui-button color="primary" size="large" @click="exploreLanguageResources">
+    <el-button type="primary" size="small" @click="exploreLanguageResources">
       explore language resources
-    </ui-button>
+    </el-button>
 
-    <ui-modal
-      ref="browseModal"
+    <el-dialog
       title="Explore Language Resources"
-      size="fullscreen"
+      :visible.sync="dialogVisible"
+      :fullscreen="true"
+      custom-class="bg-gray-100"
     >
-      <!-- modal -->
-      <div slot="header" class="text-lg">
+      <div slot="title" class="text-2xl p-4">
         <i class="fas fa-sign-language"></i>
         {{ selectedLanguage.name }}
       </div>
 
-      <!-- content -->
       <div class="flex flex-col text-base space-y-8 py-4 px-8">
-        <div class="">Also known as: {{ otherNamesAndDialects }}</div>
-        <language-summary-component />
+        <div>Also known as: {{ otherNamesAndDialects }}</div>
+        <language-summary-component class="style-language-resources-panel" />
 
-        <!-- type buttons -->
-        <div class="flex flex-col mt-8">
-          <div>Search by type:</div>
-          <div class="flex flex-row space-x-1">
-            <div v-for="(type, idx) of types" :key="idx">
-              <ui-button size="small" color="primary" @click="setType(type)">{{
-                type
-              }}</ui-button>
-            </div>
-          </div>
-        </div>
-
-        <!-- language resource autocomplete -->
-        <ui-textbox
-          ref="searchAutocomplete"
-          class="my-6"
-          placeholder="Search language resources"
-          v-model="searchText"
-          v-on:input="debouncedSearchResources"
-        >
-        </ui-textbox>
-
-        <div>Total number of matches: {{ totalMatches }}</div>
-        <!-- search results -->
-        <div class="flex flex-col space-y-2">
-          <div v-for="(result, idx) of results" :key="result.resourceName">
-            <div class="flex flex-row">
-              <div class="flex flex-col">
-                <div class="w-10">{{ idx + 1 }}.</div>
-              </div>
-              <div class="text-base flex flex-col">
-                <div class="flex-grow flex flex-row">
-                  <a
-                    :href="result.resourceLink.url"
-                    target="_name"
-                    class="text-yellow-600 hover:underline"
-                    >{{ result.resourceName }}</a
-                  >
-                </div>
-                <div class="text-sm">{{ result.type }}</div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <search-language-resources-component v-if="dialogVisible" />
       </div>
-    </ui-modal>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { flattenDeep, debounce } from "lodash";
-import Fuse from "fuse.js";
 import LanguageSummaryComponent from "./LanguageSummary.component.vue";
+import SearchLanguageResourcesComponent from "./SearchLanguageResources.component.vue";
 
 export default {
   components: {
     LanguageSummaryComponent,
+    SearchLanguageResourcesComponent,
   },
   data() {
     return {
-      fuse: undefined,
-      results: [],
-      totalMatches: undefined,
-      searchText: undefined,
+      dialogVisible: false,
       modalHeight: `${window.innerHeight * 0.8}px`,
-      debouncedSearchResources: debounce(this.searchResources, 800),
     };
   },
   computed: {
@@ -93,62 +45,22 @@ export default {
       return this.$store.state.selectedLanguage;
     },
     otherNamesAndDialects() {
-      return this.selectedLanguage.otherNamesAndDialects.join(", ");
+      return this.selectedLanguage?.otherNamesAndDialects.join(", ");
     },
     types() {
       return Object.keys(this.$store.state.selectedLanguage.summary);
     },
   },
-  mounted() {
-    this.createSearchIndex();
-  },
   methods: {
     exploreLanguageResources() {
-      this.$refs.browseModal.open();
-    },
-    createSearchIndex() {
-      let resources = this.$store.state.selectedLanguage.resources;
-      const types = Object.keys(resources);
-      resources = types.map(type => resources[type].map(e => ({ type, ...e })));
-      resources = flattenDeep(resources);
-
-      const options = {
-        threshold: 0.1,
-        ignoreLocation: true,
-        findAllMatches: true,
-        keys: [
-          "type",
-          `isOnline`,
-          `resourceName`,
-          `resourceLink.name`,
-          `resourceLink.url`,
-        ],
-      };
-      this.fuse = new Fuse(resources, options);
-    },
-    searchResources(suggestion) {
-      if (suggestion.length > 2) {
-        let results = this.fuse.search(suggestion);
-        this.results = results.slice(0, 50).map(r => r.item);
-        this.totalMatches = results.length;
-      } else {
-        this.results = [];
-      }
-    },
-    setType(type) {
-      this.searchText = type;
-      this.searchResources(type);
+      this.dialogVisible = true;
     },
   },
 };
 </script>
 
-<style lang="scss">
-// .ui-focus-container__content {
-//   height: calc(100vh * 0.8);
-// }
-
-// .style-panel {
-//   height: calc(100vh * 0.8) - 100;
-// }
+<style lang="scss" scoped>
+.style-language-resources-panel {
+  width: 500px;
+}
 </style>
---
+++ src/components/SearchLanguageResources.component.vue
@@ -0,0 +1,180 @@
+<template>
+  <div>
+    <div v-if="disableSearch" class="flex flex-col">
+      <div>
+        Search is currently disabled for this language as there are too many
+        resources. You can browse the resources at the Language Archives site:
+        <a
+          class="text-yellow-600 hover:underline"
+          target="_blank"
+          :href="selectedLanguage.dataFile"
+        >
+          {{ selectedLanguage.dataFile }}
+        </a>
+      </div>
+    </div>
+    <div v-if="!disableSearch" class="flex flex-col space-y-4">
+      <el-radio-group
+        v-model="resourcesToSearch"
+        class="flex flex-row"
+        @change="handleTypeFilterSelection"
+      >
+        <el-radio-button label="all" size="small"></el-radio-button>
+        <div v-for="(type, idx) of types" :key="idx">
+          <el-radio-button :label="type" size="small"></el-radio-button>
+        </div>
+      </el-radio-group>
+
+      <el-input
+        :clearable="true"
+        placeholder="Search for..."
+        v-model="searchText"
+        @change="debouncedSearchResources"
+        @input="debouncedSearchResources"
+        @clear="debouncedSearchResources"
+      />
+      <!-- <pre>{{ resources[0] }}</pre> -->
+
+      <div class="flex flex-row">
+        <div class="pt-1">Total: {{ totalMatches }}</div>
+
+        <el-pagination
+          class="ml-1"
+          layout="prev, pager, next"
+          :total="totalMatches"
+          :current-page="page"
+          :page-size="pageSize"
+          @current-change="setPage"
+        >
+        </el-pagination>
+      </div>
+      <!-- search results -->
+      <div class="flex flex-col space-y-2">
+        <div v-for="(result, idx) of results" :key="result.id">
+          <div class="flex flex-row">
+            <div class="flex flex-col">
+              <div class="w-10">{{ idx + 1 }}.</div>
+            </div>
+            <div class="text-base flex flex-col">
+              <div class="flex-grow flex flex-row">
+                <a
+                  :href="result.resourceLink.url"
+                  target="_name"
+                  class="text-yellow-600 hover:underline"
+                  >{{ result.resourceName }}</a
+                >
+              </div>
+              <div class="text-sm">{{ result.type }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { flattenDeep, debounce } from "lodash";
+import Fuse from "fuse.js";
+
+export default {
+  components: {},
+  data() {
+    return {
+      debouncedSearchResources: debounce(this.searchResources, 800),
+      disableSearch: false,
+      maxSearchTotal: 10000,
+      page: 1,
+      pageSize: 50,
+      resourcesToSearch: "all",
+      fuse: undefined,
+      filterType: undefined,
+      totalMatches: undefined,
+      searchText: undefined,
+      searchResults: [],
+    };
+  },
+  computed: {
+    selectedLanguage() {
+      return this.$store.state.selectedLanguage;
+    },
+    otherNamesAndDialects() {
+      return this.selectedLanguage.otherNamesAndDialects.join(", ");
+    },
+    types() {
+      return Object.keys(this.$store.state.selectedLanguage.summary);
+    },
+    resources() {
+      let resources = this.$store.state.selectedLanguage.resources;
+      const types = Object.keys(resources);
+      resources = types.map(type =>
+        resources[type].map(e => ({
+          type,
+          ...e,
+          id: `${type}_${e.resourceName}_${e.resourceLink.url}}`,
+        }))
+      );
+      return flattenDeep(resources);
+    },
+    results() {
+      let page = this.page - 1;
+      return this.searchResults.slice(
+        page * this.pageSize,
+        page * this.pageSize + this.pageSize
+      );
+    },
+  },
+  mounted() {
+    if (this.selectedLanguage.totalResources < this.maxSearchTotal) {
+      this.createSearchIndex();
+    } else {
+      this.disableSearch = true;
+    }
+  },
+  methods: {
+    createSearchIndex() {
+      const options = {
+        threshold: 0.1,
+        ignoreLocation: true,
+        findAllMatches: true,
+        includeMatches: true,
+        keys: [
+          "type",
+          `isOnline`,
+          `resourceName`,
+          `resourceLink.name`,
+          `resourceLink.url`,
+        ],
+      };
+      this.fuse = new Fuse(this.resources, options);
+      this.searchResources();
+    },
+    searchResources(suggestion) {
+      if (!suggestion) suggestion = " ";
+      let query, results;
+      if (this.resourcesToSearch === "all") {
+        results = this.fuse.search(suggestion);
+      } else {
+        query = [
+          { type: this.resourcesToSearch },
+          {
+            $or: [{ resourceName: suggestion }],
+          },
+        ];
+        results = this.fuse.search({ $and: query });
+      }
+      this.searchResults = results.map(r => r.item);
+      this.page = 1;
+      this.totalMatches = results.length;
+    },
+    handleTypeFilterSelection() {
+      this.searchResources();
+    },
+    setPage(page) {
+      this.page = page;
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
