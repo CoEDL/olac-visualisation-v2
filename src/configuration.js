a2ecf131efdad8ee2b8605f3d16e47d449475b5c
Marco La Rosa <m@lr.id.au>
2021-02-11 10:53:35 +1100

    change public map key

--- src/configuration.js
+++ src/configuration.js
@@ -1,7 +1,7 @@
 export const accessToken =
   process.env.NODE_ENV === "development"
     ? "pk.eyJ1IjoibWFyY29sYXJvc2EiLCJhIjoiY2trb3I1NHR4MDdvdTJ4cGh3YzU3eWZnZyJ9.h0IqZUqMx2sLoSQc5KM1Lg"
-    : "pk.eyJ1IjoibWFyY29sYXJvc2EiLCJhIjoiY2trb3I1NHR4MDdvdTJ4cGh3YzU3eWZnZyJ9.h0IqZUqMx2sLoSQc5KM1Lg";
+    : "pk.eyJ1IjoibmljaG9sYXN0IiwiYSI6ImNrbDAwdnZvMzBtZGYybnQ0anYxeWJjbGUifQ.ZMO9cSjOv7_-knpuvczWQg";
 
 export const mapBoxStyle = "mapbox://styles/mapbox/light-v10";
 
