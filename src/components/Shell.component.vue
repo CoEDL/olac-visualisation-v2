83ad29582b791381f06e7a0f58f625bf9196d616
Marco La Rosa <m@lr.id.au>
2021-02-12 09:45:31 +1100

    add legend bottom loft on md screens

--- src/components/Shell.component.vue
+++ src/components/Shell.component.vue
@@ -3,6 +3,10 @@
     <topbar-component class="block lg:hidden" />
     <sidebar-component class="hidden lg:block w-2/5 lg:w-1/4 h-full" />
     <map-component class="hidden md:block flex-grow" />
+    <legend-component
+      class="block lg:hidden fixed bottom-10 left-1 p-2 rounded text-xs xl:text-base bg-gray-100"
+    />
+
     <div class="block md:hidden mt-4 text-center text-sm">
       Unfortunately, a phone is just too small for this site!
     </div>
@@ -10,6 +14,7 @@
 </template>
 
 <script>
+import LegendComponent from "./Legend.component.vue";
 import MapComponent from "./Map.component.vue";
 import TopbarComponent from "./Topbar.component.vue";
 import SidebarComponent from "./Sidebar.component.vue";
@@ -18,6 +23,7 @@ export default {
     TopbarComponent,
     SidebarComponent,
     MapComponent,
+    LegendComponent,
   },
   data() {
     return {};
