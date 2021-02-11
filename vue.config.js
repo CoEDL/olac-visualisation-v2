91ae118bc89ce775bd6a75b91ecfda01419230ac
Marco La Rosa <m@lr.id.au>
2021-02-11 16:23:33 +1100

    set cors headers

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/olacvis/" : "/",
   configureWebpack: {
+    devServer: {
+      headers: { "Access-Control-Allow-Origin": "*" },
+    },
     plugins: [
       new CopyPlugin({
         patterns: [{ from: "data", to: "repository" }],
