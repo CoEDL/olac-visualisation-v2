410a5cb3ae97a58ee06484d8d89b589cd35bad4f
Marco La Rosa <m@lr.id.au>
2021-04-21 11:49:00 +1000

    fix mode and add dev server config

--- webpack-development.js
+++ webpack-development.js
@@ -3,7 +3,15 @@ const merge = require("webpack-merge");
 const common = require("./webpack-common");
 
 const configuration = merge(common, {
-  mode: "production",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    host: "0.0.0.0",
+    port: 8080,
+    historyApiFallback: true,
+    writeToDisk: true,
+  },
 });
 
 module.exports = configuration;
