f97383eb6c9bcdcb85b3fa8a2cd547fcafaa067b
Marco La Rosa <m@lr.id.au>
2021-04-23 12:04:24 +1000

    revise output format

--- webpack-common.js
+++ webpack-common.js
@@ -11,7 +11,7 @@ module.exports = {
   entry: ["./src/main.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].[fullhash].bundle.js",
+    filename: "[contenthash].js",
     publicPath: "/",
   },
   plugins: [
@@ -23,7 +23,7 @@ module.exports = {
       template: "./public/index.html",
     }),
     new VueLoaderPlugin(),
-    new MiniCssExtractPlugin({ filename: "[name].[fullhash].bundle.css" }),
+    new MiniCssExtractPlugin({ filename: "[contenthash].css" }),
   ],
   module: {
     rules: [
